05350a0fe238e530262b0a67939b7e4300bd9359
Laura Pérez Mayos <lpmayos@gmail.com>
2016-12-20 20:52:18 +0100

    corrected graph legend in task 4; nicer output task 5 (with png example)

--- Week1/task5.m
+++ Week1/task5.m
@@ -12,13 +12,24 @@ addpath('../utils');
 addpath('../week1');
 
 % First, show color representation of optical flow
-filename = '000000_10.png';
+filename = '000019_10.png';
 original_image = imread(strcat('../datasets/KITTI_devkit/data_stereo_flow/training/image_0/', filename));
 F = flow_read(strcat('../datasets/KITTI_devkit/data_stereo_flow/training/flow_noc/', filename));  % loads flow field F from png file
 optical_flow_image = flow_to_color(F);  % computes color representation of optical flow field
 
-figure(1);
-subplot(3,1,1); imshow(original_image); title('Original Image');
-subplot(3,1,2); imshow(optical_flow_image); title('Color representation of optical flow field');
-subplot(3,1,3); quiver(F(:,:,1), F(:,:,2)); title('Quiver plot');
+k = 10;
+[x,y] = meshgrid(1:k:size(original_image,2),1:k:size(original_image,1));
+xvec = 1:k:size(original_image,2);
+yvec = 1:k:size(original_image,1);
+u = F(yvec,xvec,1);
+v = F(yvec,xvec,2);
+
+% figure(1);
+% subplot(3,1,1); imshow(original_image); title('Original Image');
+% subplot(3,1,2); imshow(optical_flow_image); title('Color representation of optical flow field');
+% subplot(3,1,3); quiver(x, y, u, v); title('Quiver plot');
+
+figure(2)
+imshow(original_image); hold on
+quiver(x, y, u, v, 2);
 
