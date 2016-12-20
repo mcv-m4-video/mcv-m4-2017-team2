6f7955db935ec7dad9909237ad80ebf0d917511e
cristinabustos16 <mcb9216@gmail.com>
2016-12-20 23:52:06 +0100

    task1 saving binary masks

--- Week1/task1.m
+++ Week1/task1.m
@@ -16,13 +16,15 @@ TP_B = 0; FP_B = 0; FN_B = 0; TN_B = 0;
 motion = 170;
 
 for i=1:files_number
-    file_class  = results_files(i).name(6);
+    file_class  = results_files(i).name(6); %A or B
     file_number = results_files(i).name(8:13);  % example: take '001201' from 'test_A_001201.png'
     
     if file_class == 'A'
         gt_A = imread(strcat(dirGT,'gt',file_number,'.png'));  % Read the A image
         test_A = imread(strcat(dirResults,'test_A_',file_number,'.png'));  % Read the GT image
         gt_A = gt_A >= motion;  % binarize gt mask
+        imwrite(gt_A*255,strcat(dirGT,'/masks/gt',file_number,'.png'));
+        imwrite(test_A*255,strcat(dirResults,'/masks/',results_files(i).name));
         [TP, TN, FP, FN] = get_metrics(gt_A, test_A);
         TP_A = TP_A + TP;
         FP_A = FP_A + FP;
@@ -32,6 +34,7 @@ for i=1:files_number
         gt_B = imread(strcat(dirGT,'gt',file_number,'.png'));  % Read the B image
         test_B = imread(strcat(dirResults,'test_B_',file_number,'.png'));  % Read the GT image
         gt_B = gt_B >= motion;  % binarize gt mask
+        imwrite(test_B*255,strcat(dirResults,'/masks/',results_files(i).name));
         [TP, TN, FP, FN] = get_metrics (gt_B, test_B);
         TP_B = TP_B + TP;
         FP_B = FP_B + FP;
