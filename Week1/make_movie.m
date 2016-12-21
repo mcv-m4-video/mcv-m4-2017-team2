847ead576de1ddcdc21b41e9bd31af2ed4e16596
xianlopez <lopezalvarez.xian@gmail.com>
2016-12-21 10:34:16 +0100

    Task 2
    A bug in make_movie has been corrected, too.

--- Week1/make_movie.m
+++ Week1/make_movie.m
@@ -17,30 +17,35 @@ open(v);
 fig = figure();
 
 for i = 1:nfiles
-    
     file_class  = results_files(i).name(6);
     file_number = results_files(i).name(8:13);  % example: take '001201' from 'test_A_001201.png'
-    
-    input_image = imread(strcat(dirinput,'in',file_number,'.jpg')); % Read the input image
-    gt_image = imread(strcat(dirGT,'gt',file_number,'.png'));  % Read the ground truth image
-    test_A = imread(strcat(dirResults,'test_A_',file_number,'.png'));  % Read the A image
-    test_B = imread(strcat(dirResults,'test_B_',file_number,'.png'));  % Read the B image
-    
-    subplot(2,2,1)
-    imshow(input_image)
-    title(['in',file_number,'.jpg'])
-    subplot(2,2,2)
-    imshow(gt_image)
-    title('Ground Truth')
-    subplot(2,2,3)
-    imshow(test_A, [0 1])
-    title('Test A')
-    subplot(2,2,4)
-    imshow(test_B, [0 1])
-    title('Test B')
         
-    frame = getframe(fig);
-    writeVideo(v,frame);
+    % We have the double files than real frames, for we have the results of
+    % two tests. In order to avoid making a video with double length, we
+    % consider only those images of test A:
+    if(file_class == 'A')
+
+        input_image = imread(strcat(dirinput,'in',file_number,'.jpg')); % Read the input image
+        gt_image = imread(strcat(dirGT,'gt',file_number,'.png'));  % Read the ground truth image
+        test_A = imread(strcat(dirResults,'test_A_',file_number,'.png'));  % Read the A image
+        test_B = imread(strcat(dirResults,'test_B_',file_number,'.png'));  % Read the B image
+
+        subplot(2,2,1)
+        imshow(input_image)
+        title(['in',file_number,'.jpg'])
+        subplot(2,2,2)
+        imshow(gt_image)
+        title('Ground Truth')
+        subplot(2,2,3)
+        imshow(test_A, [0 1])
+        title('Test A')
+        subplot(2,2,4)
+        imshow(test_B, [0 1])
+        title('Test B')
+
+        frame = getframe(fig);
+        writeVideo(v,frame);
+    end
 end
 
 close(v);
--- Week1/task2.m
+++ Week1/task2.m
@@ -9,7 +9,7 @@ dirGT = '../datasets/cdvd/dataset/baseline/highway/groundtruth/';
 dirResults = '../datasets/cdvd/dataset/baseline/highway/results/';
 
 results_files = list_files(dirResults);
-files_number = size(results_files,1);
+nfiles = size(results_files,1);
 
 TP_A = zeros(1,200);
 FP_A = zeros(1,200);
@@ -27,6 +27,8 @@ precision_B = zeros(1,200);
 recall_B = zeros(1,200);
 F1_B = zeros(1,200);
 
+TFG = zeros(1,200); % Total Foreground pixels.
+
 count_A = 0;
 count_B = 0;
 
@@ -39,25 +41,25 @@ count_B = 0;
 
 motion = 170;
 
-for i=1:files_number
+for i = 1:nfiles
     file_class  = results_files(i).name(6);
     file_number = results_files(i).name(8:13);%example: take '001201' from 'test_A_001201.png'
+    gt_im = imread(strcat(dirGT,'gt',file_number,'.png')); % Read the GT image
     
     if file_class == 'A'
         count_A = count_A + 1;
-        gt_A = imread(strcat(dirGT,'gt',file_number,'.png')); % Read the GT image
         test_A = imread(strcat(dirResults,'test_A_',file_number,'.png')); % Read the A image
-        gt_A = gt_A >= motion; %binarize gt mask
-        [TP_A(count_A), TN_A(count_A), FP_A(count_A), FN_A(count_A)] = get_metrics (gt_A, test_A);
+        gt_im = gt_im >= motion; %binarize gt mask
+        [TP_A(count_A), TN_A(count_A), FP_A(count_A), FN_A(count_A)] = get_metrics (gt_im, test_A);
         [precision_A(count_A), recall_A(count_A), F1_A(count_A)] = ...
             evaluation_metrics(TP_A(count_A), TN_A(count_A), FP_A(count_A), FN_A(count_A));
+        TFG(count_A) = sum(sum(gt_im));
         
     else % B
         count_B = count_B + 1;
-        gt_B = imread(strcat(dirGT,'gt',file_number,'.png')); % Read the GT image
         test_B = imread(strcat(dirResults,'test_B_',file_number,'.png')); % Read the B image
-        gt_B = gt_B >= motion; %binarize gt mask
-        [TP_B(count_B), TN_B(count_B), FP_B(count_B), FN_B(count_B)] = get_metrics (gt_B, test_B);
+        gt_im = gt_im >= motion; %binarize gt mask
+        [TP_B(count_B), TN_B(count_B), FP_B(count_B), FN_B(count_B)] = get_metrics (gt_im, test_B);
         [precision_B(count_B), recall_B(count_B), F1_B(count_B)] = ...
             evaluation_metrics(TP_B(count_B), TN_B(count_B), FP_B(count_B), FN_B(count_B));
     end
@@ -65,35 +67,25 @@ end
 
 frames = [1201:1400];
 
-max_precision = max([precision_A, precision_B]);
-min_precision = min([precision_A, precision_B]);
-
-max_recall = max([recall_A, recall_B]);
-min_recall = min([recall_A, recall_B]);
-
 max_F1 = max([F1_A, F1_B]);
 min_F1 = min([F1_A, F1_B]);
 
 figure(1)
-plot(frames, precision_A, 'b', 'LineWidth', 2)
-hold on
-plot(frames, precision_B, 'k', 'LineWidth', 2)
-title('Precision')
-axis([1201 1400 min_precision max_precision])
-
-figure(2)
-plot(frames, recall_A, 'b', 'LineWidth', 2)
-hold on
-plot(frames, recall_B, 'k', 'LineWidth', 2)
-title('Recall')
-axis([1201 1400 min_recall max_recall])
-
-figure(3)
 plot(frames, F1_A, 'b', 'LineWidth', 2)
 hold on
-plot(frames, F1_B, 'k', 'LineWidth', 2)
+plot(frames, F1_B, 'r', 'LineWidth', 2)
 title('F1')
+legend('F1 test A', 'F1 test B', 'Location', 'southeast')
 axis([1201 1400 min_F1 max_F1])
+
+figure(2)
+plot(frames, TFG, 'k', 'LineWidth', 2)
+hold on
+plot(frames, TP_A, 'b', 'LineWidth', 2)
+plot(frames, TP_B, 'r', 'LineWidth', 2)
+title('F1')
+legend('Total Foreground pixels', 'True Positives test A', 'True Positives test B', 'North', 'Location', 'North')
+axis([1201 1400 0 max(TFG)])
 
 
 
