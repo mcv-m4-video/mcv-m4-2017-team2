d298997df09bdadef492450454bc193ed7009bf6
cristinabustos16 <mcb9216@gmail.com>
2017-01-10 03:49:13 +0100

    NaN values correction in precision and recall

--- utils/evaluation_metrics.m
+++ utils/evaluation_metrics.m
@@ -3,6 +3,8 @@ function [precision,recall, F1] = evaluation_metrics(TP,TN,FP,FN)
 	precision=TP./(TP+FP);
 	recall=TP./(TP+FN);
     F1=2*precision.*recall./(precision+recall);
+    precision(isnan(precision)) = 0;
+    recall(isnan(recall)) = 0;
     F1(isnan(F1)) = 0; 
 
 end
