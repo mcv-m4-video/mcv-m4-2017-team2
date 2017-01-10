302598999928f6ed79159efc71d1b8fd674569bc
GonzaBCCC <gonzabenito@gmail.com>
2017-01-10 10:52:53 +0100

    graf

--- week2/for_printing.m
+++ week2/for_printing.m
@@ -4,3 +4,21 @@ title('F1 for the 3 datasets');
 xlabel('alpha');
 ylabel('F1 measure');
 legend('fall','traffic','highway');
+
+indexmaxfall = find(max(F1_fall) == F1_fall);
+xmaxfall = alpha_vect(indexmaxfall);
+ymaxfall = F1_fall(indexmaxfall);
+strmaxf = ['Max alpha = ',num2str(xmaxfall)];
+text(xmaxfall,ymaxfall,strmaxf,'HorizontalAlignment','left');
+
+indexmaxtraf = find(max(F1_traffic) == F1_traffic);
+xmaxtraf = alpha_vect(indexmaxtraf);
+ymaxtraf = F1_traffic(indexmaxtraf);
+strmaxt = ['Max alpha = ',num2str(xmaxtraf)];
+text(xmaxtraf,ymaxtraf,strmaxt,'HorizontalAlignment','left');
+
+indexmaxh = find(max(F1_highway) == F1_highway);
+xmaxh = alpha_vect(indexmaxh);
+ymaxh = F1_highway(indexmaxh);
+strmaxh = ['Max alpha = ',num2str(xmaxh)];
+text(xmaxh,ymaxh,strmaxh,'HorizontalAlignment','left');
