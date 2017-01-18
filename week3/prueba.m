b1007d57d4f26d7ddcac5088b2f144fa5a435aee
xianlopez <lopezalvarez.xian@gmail.com>
2017-01-18 13:46:09 +0100

    nothing

--- week3/prueba.m
+++ week3/prueba.m
@@ -13,7 +13,6 @@ useTrain = 1;
 dirinput = strcat('./task3_results/', videoname, '/');
 sequence = double(read_sequence(dirinput));
 
-
 % Evaluate detection:
 [precision, recall, F1] = test_sequence_2val(sequence, videoname, show_video, write_video, filename, useTrain, size(sequence,3));
 fprintf('Precision: %f\n', precision)
