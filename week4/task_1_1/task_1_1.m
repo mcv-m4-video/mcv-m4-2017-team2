df632adb3705748e1a89b83ae57bc61ec6534572
GonzaBCCC <gonzabenito@gmail.com>
2017-01-27 21:25:51 +0100

    creation of task_1_1 folder

---
+++ week4/task_1_1/task_1_1.m
@@ -0,0 +1,17 @@
+function task_1_1(block_size, comp_dir)
+
+close all
+addpath('../datasets');
+addpath('../datasets/KITTI_devkit');
+addpath('../utils');
+addpath('../week4');
+
+
+
+
+
+
+
+
+    
+end
