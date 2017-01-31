e08831eb1c8f9f0eaa3b420507f4b1a2e7a210de
GonzaBCCC <gonzabenito@gmail.com>
2017-01-31 02:57:45 +0100

    task1_1 arreglado
    block matching funcionando y resultados para kitti seq 45

--- week4/task1_1.m
+++ week4/task1_1.m
@@ -8,7 +8,7 @@ addpath('../utils');
 addpath('../week1');
 
 %for sequence 45
-seq_id = 45;
+%seq_id = 45;
 if seq_id == 45
     gt = flow_read('../datasets/KITTI_devkit/data_stereo_flow/training/flow_noc/000045_10.png');
     img1 = imread('../datasets/KITTI_devkit/data_stereo_flow/training/image_0/000045_10.png');
@@ -21,14 +21,14 @@ elseif seq_id == 157
 end
 
 
-% block_size = 20;
-% search_area = 20;
+%block_size = 40;
+%search_area = 20;
 % optical flow using block matching
 [flow_estimation_x, flow_estimation_y]  = block_matching(img1, img2, block_size, search_area);
 
 %combine the flow_estimations to calculate error using kitti dev functions
-flow_estimated(:,:,1) = flow_estimation_x;
-flow_estimated(:,:,2) = flow_estimation_y;
+flow_estimated(:,:,2) = flow_estimation_x;
+flow_estimated(:,:,1) = flow_estimation_y;
 
 %get the error mat between the estimated motion and gt motion: 
 [error,F_gt_val] = flow_error_map (gt,flow_estimated);
@@ -42,7 +42,7 @@ pepn = length(find(error>3))/length(find(F_gt_val));
 % fprintf('\t\tWEEK 4 TASK 1.1 RESULTS\n');
 % fprintf('Sequence\t\tMSEN\t\tPEPN\n');
 % fprintf('--------------------------------------------------\n');
-% fprintf(['Seq ',num2str(seq_id),'\t\t', num2str(msen), '\t\t', num2str(pepn*100),'\n']);
+% fprintf(['Seq ',num2str(seq_id),'\t\t', num2str(msen), '\t\t', num2str(pepn),'\n']);%*100),'\n']);
 end
 
 
