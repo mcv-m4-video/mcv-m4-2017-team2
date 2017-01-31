d86ff0a5bfb673ac5816a57f9cd9f341d86c8411
Laura Pérez Mayos <lpmayos@gmail.com>
2017-02-01 00:46:38 +0100

    vidoe stabilization grid search

--- week4/videoStabilizationPipeline.m
+++ week4/videoStabilizationPipeline.m
@@ -22,7 +22,7 @@ function videoStabilizationPipeline
     %% load data
     % Datasets to use: 'highway', 'fall', 'traffic', 'traffic_stabilized_target_tracking'
     % Choose dataset images to work on from the above:
-    data = 'traffic';
+    data = 'traffic_stabilized_target_tracking';
     [start_img, range_images, dirInputs, dirGT] = load_data(data);
     input_files = list_files(dirInputs);
 
@@ -44,20 +44,22 @@ function videoStabilizationPipeline
     background = 55;
     foreground = 255;
 
-    % alpha_vect = 0:0.25:8;
-    alpha_vect = [2.5];  % traffic_stabilized_target_tracking
-    % alpha_vect = [2.5];  % traffic
+    alpha_vect = 0:0.25:8;
+    rho_vect = 0.025:0.025:1;
 
     [mu_matrix, sigma_matrix] = train_background(start_img, range_images, input_files, dirInputs);
 
-    [time, AUC, TP_, TN_, FP_, FN_, precision, recall, F1] = alpha_sweep(data, alpha_vect, mu_matrix, sigma_matrix, range_images, start_img, dirInputs, input_files, background, foreground, dirGT, rho_val)
+    [time, AUC, TP_, TN_, FP_, FN_, precision, recall, F1] = alpha_sweep(data, alpha_vect, mu_matrix, sigma_matrix, range_images, start_img, dirInputs, input_files, background, foreground, dirGT, rho_vect);
 end
 
 
-function [time, AUC, TP_, TN_, FP_, FN_, precision, recall, F1] = alpha_sweep(data, alpha_vect, mu_matrix, sigma_matrix, range_images, start_img, dirInputs, input_files, background, foreground, dirGT, rho_val)
+function [time, AUC, TP_, TN_, FP_, FN_, precision, recall, F1] = alpha_sweep(data, alpha_vect, mu_matrix, sigma_matrix, range_images, start_img, dirInputs, input_files, background, foreground, dirGT, rho_vect)
 %function for sweeping through several thresholds to compare performance
 
     tic
+
+    plot_detection = false;
+    plot_graphs = false;
 
     precision = zeros(1,size(alpha_vect,2));
     recall = zeros(1,size(alpha_vect,2));
@@ -68,73 +70,89 @@ function [time, AUC, TP_, TN_, FP_, FN_, precision, recall, F1] = alpha_sweep(da
     FP_ = [];
     FN_ = [];
 
+    max_f1 = 0;
+    max_alpha = 0;
+    max_rho = 0;
+
     for n=1:size(alpha_vect,2)
-        
         alpha = alpha_vect(n);
+        for rho = rho_vect
         
-        %Metrics for alpha sweep
-        TP_global = 0;
-        TN_global = 0;
-        FP_global = 0;
-        FN_global = 0;
-        
-        %detect foreground and compare results
-        for i=1:(round(range_images/2)+1)
-            index = i + (start_img + range_images/2) - 1;
-            file_number = input_files(index).name(3:8);
-            try
-                img = double(rgb2gray(imread(strcat(dirInputs,'in',file_number,'.jpg'))));
-            catch
-                img = double(imread(strcat(dirInputs,'in',file_number,'.jpg')));
+            %Metrics for alpha sweep
+            TP_global = 0;
+            TN_global = 0;
+            FP_global = 0;
+            FN_global = 0;
+            
+            %detect foreground and compare results
+            for i=round(range_images/2)+2:range_images
+                index = i + start_img -1;
+                file_number = input_files(index).name(3:8);
+                try
+                    img = double(rgb2gray(imread(strcat(dirInputs,'in',file_number,'.jpg'))));
+                catch
+                    img = double(imread(strcat(dirInputs,'in',file_number,'.jpg')));
+                end
+
+                test_backg_in(:,:,i) = img;
+
+                detection(:,:,i) = double((abs(test_backg_in(:,:,i)-mu_matrix) >= (alpha * (sigma_matrix + 2))));
+
+                % fill holes (week 3 task 1)
+                connectivity = 4;  % can be either 4 or 8
+                detection(:,:,i) = imfill(detection(:,:,i), connectivity);
+
+                % remove noise (week 3 task 2)
+                % not applied because we said it does not change the results for traffic sequence
+                detection(:,:,i) = bwareaopen(detection(:,:,i),500);
+
+                % apply morphological operators  (week 3 task 3)
+                se = strel('square',15);
+                detection(:,:,i) = imopen(detection(:,:,i),se);
+                detection(:,:,i) = imclose(detection(:,:,i),se);
+
+
+                gt = imread(strcat(dirGT,'gt',file_number,'.png'));
+                gt_back = gt <= background;
+                gt_fore = gt >= foreground;
+
+                if plot_detection
+                    fig = figure(10);
+                    subplot(1,2,1); imshow(gt_fore); title('ground truth');
+                    subplot(1,2,2); imshow(detection(:,:,i)); title('detection');
+                    % pause();
+                end
+
+                [TP, TN, FP, FN] = get_metrics_2val(gt_back, gt_fore, detection(:,:,i), data);
+
+                %option of getting overall metrics
+                TP_global = TP_global + TP;
+                TN_global = TN_global + TN;
+                FP_global = FP_global + FP;
+                FN_global = FN_global + FN;
+
+                % adapt model using pixels belonging to the background
+                [mu_matrix, sigma_matrix] = adaptModel(test_backg_in(:,:,i), detection(:,:,i), mu_matrix, sigma_matrix, rho);
+
             end
 
-            test_backg_in(:,:,i) = img;
+            %global metrics for threshold sweep:
+            [precision(n), recall(n), F1(n)] = evaluation_metrics(TP_global,TN_global,FP_global,FN_global);
+            TP_(n) = TP_global;
+            TN_(n) = TN_global;
+            FP_(n) = FP_global;
+            FN_(n) = FN_global;
 
-            detection(:,:,i) = double((abs(test_backg_in(:,:,i)-mu_matrix) >= (alpha * (sigma_matrix + 2))));
-
-            % fill holes (week 3 task 1)
-            connectivity = 4;  % can be either 4 or 8
-            detection(:,:,i) = imfill(detection(:,:,i), connectivity);
-
-            % remove noise (week 3 task 2)
-            % not applied because we said it does not change the results for traffic sequence
-            detection(:,:,i) = bwareaopen(detection(:,:,i),500);
-
-            % apply morphological operators  (week 3 task 3)
-            se = strel('square',15);
-            detection(:,:,i) = imopen(detection(:,:,i),se);
-            detection(:,:,i) = imclose(detection(:,:,i),se);
-
-
-            gt = imread(strcat(dirGT,'gt',file_number,'.png'));
-            gt_back = gt <= background;
-            gt_fore = gt >= foreground;
-
-            fig = figure(10);
-            subplot(1,2,1); imshow(gt_fore); title('ground truth');
-            subplot(1,2,2); imshow(detection(:,:,i)); title('detection');
-            % pause();
-
-            [TP, TN, FP, FN] = get_metrics_2val(gt_back, gt_fore, detection(:,:,i), data);
-
-            %option of getting overall metrics
-            TP_global = TP_global + TP;
-            TN_global = TN_global + TN;
-            FP_global = FP_global + FP;
-            FN_global = FN_global + FN;
-
-            % adapt model using pixels belonging to the background
-            [mu_matrix, sigma_matrix] = adaptModel(test_backg_in(:,:,i), detection(:,:,i), mu_matrix, sigma_matrix, rho_val);
+            if mean(F1(n)) > max_f1
+                max_f1 = mean(F1(n));
+                max_alpha = alpha;
+                max_rho = rho;
+                fprintf(['max f1 = \t', num2str(max_f1),'\n']);
+                fprintf(['max alpha = \t', num2str(max_alpha),'\n']);
+                fprintf(['max rho = \t', num2str(max_rho),'\n']);
+            end
 
         end
-
-        %global metrics for threshold sweep:
-        [precision(n), recall(n), F1(n)] = evaluation_metrics(TP_global,TN_global,FP_global,FN_global);
-        TP_(n) = TP_global;
-        TN_(n) = TN_global;
-        FP_(n) = FP_global;
-        FN_(n) = FN_global;
-
     end    
 
     time = toc;
@@ -142,27 +160,29 @@ function [time, AUC, TP_, TN_, FP_, FN_, precision, recall, F1] = alpha_sweep(da
     %AUC of Precision metrics
     AUC = trapz(precision,2)/size(TP_,2);
 
-    x = alpha_vect;
-    figure(1)
-    plot(x, transpose(precision), 'b', x, transpose(recall), 'r',  x, transpose(F1), 'k');
-    title(strcat({'Precision, Recall & F1 vs Threshold for dataset '},data));
-    xlabel('Threshold');
-    ylabel('Measure');
-    legend('Precision','Recall','F1');
+    if plot_graphs
+        x = alpha_vect;
+        figure(1)
+        plot(x, transpose(precision), 'b', x, transpose(recall), 'r',  x, transpose(F1), 'k');
+        title(strcat({'Precision, Recall & F1 vs Threshold for dataset '},data));
+        xlabel('Threshold');
+        ylabel('Measure');
+        legend('Precision','Recall','F1');
 
-    figure(2)
-    plot(x, transpose(TP_),'b', x, transpose(TN_),'g', x, transpose(FP_),'r', x, transpose(FN_));
-    title(strcat({'TP, TN, FP & FN vs Threshold for '},data));
-    xlabel('Threshold');
-    ylabel('Pixels');
-    legend('TP','TN','FP','FN');
+        figure(2)
+        plot(x, transpose(TP_),'b', x, transpose(TN_),'g', x, transpose(FP_),'r', x, transpose(FN_));
+        title(strcat({'TP, TN, FP & FN vs Threshold for '},data));
+        xlabel('Threshold');
+        ylabel('Pixels');
+        legend('TP','TN','FP','FN');
 
-    figure(3)
-    plot(recall, transpose(precision), 'g');
-    title(strcat({'Recall vs Precision & AUC for dataset '},data));
-    xlabel('Recall');
-    ylabel('Precision');
-    legend('Recall vs Precision','Area under the curve');
+        figure(3)
+        plot(recall, transpose(precision), 'g');
+        title(strcat({'Recall vs Precision & AUC for dataset '},data));
+        xlabel('Recall');
+        ylabel('Precision');
+        legend('Recall vs Precision','Area under the curve');
+    end
 end
 
 
